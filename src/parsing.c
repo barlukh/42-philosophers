2189b365c1aa61a257752c24231360ee127c1333
Boris Gazur <barlukh@gmail.com>
2025-07-04 14:17:26 +0300

    Rework ERROR macro

--- src/parsing.c
+++ src/parsing.c
@@ -6,7 +6,7 @@
 /*   By: bgazur <bgazur@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/02 16:31:37 by bgazur            #+#    #+#             */
-/*   Updated: 2025/07/03 13:57:40 by bgazur           ###   ########.fr       */
+/*   Updated: 2025/07/04 13:43:33 by bgazur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,15 @@ static int	ft_atoi(const char *s);
 int	parse_arguments(int argc, char **argv, t_data *data)
 {
 	if (check_argc(argc) == EXIT_FAILURE)
+	{
+		printf("Invalid arguments\n");
 		return (EXIT_FAILURE);
+	}
 	else if (convert_argv(argc, argv, data) == EXIT_FAILURE)
+	{
+		printf("Invalid arguments\n");
 		return (EXIT_FAILURE);
+	}
 	return (EXIT_SUCCESS);
 }
 
@@ -37,21 +43,21 @@ static int	check_argc(int argc)
 static int	convert_argv(int argc, char **argv, t_data *data)
 {
 	data->philo_count = ft_atoi(argv[1]);
-	if (data->philo_count < 1)
+	if (data->philo_count == ERROR)
 		return (EXIT_FAILURE);
 	data->tt_die = ft_atoi(argv[2]);
-	if (data->tt_die < 1)
+	if (data->tt_die == ERROR)
 		return (EXIT_FAILURE);
 	data->tt_eat = ft_atoi(argv[3]);
-	if (data->tt_eat < 1)
+	if (data->tt_eat == ERROR)
 		return (EXIT_FAILURE);
 	data->tt_sleep = ft_atoi(argv[4]);
-	if (data->tt_sleep < 1)
+	if (data->tt_sleep == ERROR)
 		return (EXIT_FAILURE);
 	if (argc == 6)
 	{
 		data->must_eat = ft_atoi(argv[5]);
-		if (data->must_eat < 1)
+		if (data->must_eat == ERROR)
 			return (EXIT_FAILURE);
 	}
 	else
